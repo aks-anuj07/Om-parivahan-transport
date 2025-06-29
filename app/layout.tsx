7cb92b681525855efd4768965f066d71fa757acd
https://github.com/Yashvir835/Prakartik-Ahaar <singhyashvir835@gmail.com>
2025-06-29 10:31:18 +0530

    added logo in the metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ export const metadata: Metadata = {
   title: 'OM PARIVAHAN SERVICES',
   description: 'OM PARIVAHAN SERVICES is a leading logistics and transportation company specializing in efficient and reliable delivery solutions across India.',
   generator: 'Yashvir835',
+  icons: {
+    icon: '/logo.svg',         // default icon
+    shortcut: '/logo.svg ',     // optional for browser shortcut
+    apple: '/logo.svg',         // optional for Apple devices
+  },
 }
 
 export default function RootLayout({
